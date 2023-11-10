56ae2318685121e631ad398319d926d303cb9c40
Tayte Boss <boss.tayte@gmail.com>
2023-11-10 10:27:56 +0000

    [style] Info Page - Structure and responsive styling

--- frontend/components/blocks/Capabilities/Capabilities.tsx
+++ frontend/components/blocks/Capabilities/Capabilities.tsx
@@ -65,7 +65,7 @@ const Capabilities = (props: Props) => {
 					/>
 				))}
 			</LayoutWrapper>
-			<Divider />
+			<Divider className="capabilities__divider" />
 		</CapabilitiesWrapper>
 	);
 };
---
+++ frontend/pages/info.tsx
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+import client from '../client';
+import { homePageQueryString, infoPageQueryString } from '../queries';
+import { motion } from 'framer-motion';
+import { NextSeo } from 'next-seo';
+import PageHeroTitle from '../components/blocks/PageHeroTitle';
+import Capabilities from '../components/blocks/Capabilities';
+import { HomePageType } from '../shared/types/types';
+
+type Props = {
+	data: any;
+	pageTransitionVariants: any;
+	homeData: HomePageType;
+};
+
+const PageWrapper = styled(motion.div)`
+	.capabilities {
+		&__divider {
+			display: none;
+		}
+	}
+`;
+
+const Page = (props: Props) => {
+	const {
+		data,
+		homeData,
+		pageTransitionVariants
+	} = props;
+
+	console.log('data', data);
+
+	return (
+		<PageWrapper
+			variants={pageTransitionVariants}
+			initial='hidden'
+			animate='visible'
+			exit='hidden'
+		>
+			<NextSeo
+				title={data?.seoTitle || 'Aka'}
+				description={data?.seoDescription || ''}
+			/>
+			<PageHeroTitle title={data?.introHeading} />
+			<Capabilities data={homeData?.capabilities} />
+		</PageWrapper>
+	);
+};
+
+export async function getStaticProps() {
+	const data = await client.fetch(infoPageQueryString);
+	const homeData = await client.fetch(homePageQueryString);
+
+	return {
+		props: {
+			data,
+			homeData
+		},
+	};
+}
+
+export default Page;
